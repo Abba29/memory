f76042ed1c8cbd5705c09ba7f953ba1bac5d8290
Lorenzo Abbadini <lory369@gmail.com>
2020-09-12 12:24:31 +0200

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Metodo che da' inizio alla partita (caricando la relativa scena)
+    public void NuovaPartita()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Metodo che consente di uscire dal gioco
+    public void Esci()
+    {
+        Application.Quit();
+    }
+}
